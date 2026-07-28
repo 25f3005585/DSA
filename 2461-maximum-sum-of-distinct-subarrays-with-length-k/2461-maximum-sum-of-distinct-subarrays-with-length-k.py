95c1c62cabb067412965e131f52d8e6db68b55e0
Ayush Dixit <fsayush100@gmail.com>
2026-07-28 19:22:08 +0530

    Time: 99 ms (81.16%), Space: 37.3 MB (40.89%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,38 +1,29 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        n = len(nums)
         hash_map = {}
         curr_sum = 0
 
         for i in range(k):
-            element = nums[i]
             curr_sum += nums[i]
-            if element in hash_map:
-                hash_map[element] += 1
-            else:
-                hash_map[element] = 1
-        
+            hash_map[nums[i]] = hash_map.get(nums[i], 0) + 1
+
         max_sum = 0
         if len(hash_map) == k:
             max_sum = curr_sum
-        
-        for i in range(k, n):
-            left = nums[i-k]
-            
-            if hash_map[left] == 1:
-                del hash_map[left]      
-            else:
-                hash_map[left] -=1
 
+        for i in range(k, len(nums)):
+            left = nums[i - k]
             right = nums[i]
 
-            if right in hash_map:
-                hash_map[right] += 1
-            else:
-                hash_map[right] = 1
-            
-            curr_sum = curr_sum - left + right
+            curr_sum += right - left
+
+            hash_map[left] -= 1
+            if hash_map[left] == 0:
+                del hash_map[left]
+
+            hash_map[right] = hash_map.get(right, 0) + 1
+
             if len(hash_map) == k:
-                max_sum = max(max_sum, curr_sum) 
-            
+                max_sum = max(max_sum, curr_sum)
+
         return max_sum
