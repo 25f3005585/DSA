5920dd95fd978d151e285e7484b8fb750a8539fa
Ayush Dixit <fsayush100@gmail.com>
2026-07-31 12:58:21 +0530

    Time: 49 ms (5.62%), Space: 30.8 MB (75.93%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,4 +1,28 @@
 class Solution:
+    # def maxScore(self, nums: List[int], k: int) -> int:
+    #     n = len(nums)
+
+    #     if k == n:
+    #         return sum(nums)
+
+    #     if k == 1:
+    #         if nums[0] > nums[n - 1]:
+    #             return nums[0]
+    #         else:
+    #             return nums[n - 1]
+
+    #     curr_sum = 0
+    #     for i in range(n - k, n):
+    #         curr_sum += nums[i]
+
+    #     max_sum = curr_sum
+
+    #     for i in range(n, n + k):
+    #         curr_sum += nums[i % n] - nums[(i - k) % n]
+    #         max_sum = max(max_sum, curr_sum)
+
+    #     return max_sum
+
     def maxScore(self, nums: List[int], k: int) -> int:
         n = len(nums)
 
@@ -12,13 +36,17 @@ class Solution:
                 return nums[n - 1]
 
         curr_sum = 0
-        for i in range(n - k, n):
-            curr_sum += nums[i]
+        left = n - k
+        max_sum = float("-inf")
 
-        max_sum = curr_sum
+        for right in range(n - k, n + k):
+            curr_sum += nums[right % n]
 
-        for i in range(n, n + k):
-            curr_sum += nums[i % n] - nums[(i - k) % n]
-            max_sum = max(max_sum, curr_sum)
+            if right - left + 1 > k:
+                curr_sum -= nums[left % n]
+                left += 1
+
+            if right - left + 1 == k:
+                max_sum = max(max_sum, curr_sum)
 
         return max_sum
