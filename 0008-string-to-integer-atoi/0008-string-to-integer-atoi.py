111df29138eadfde1e13c3dbbb5d6135f16bb276
Ayush Dixit <fsayush100@gmail.com>
2026-08-02 19:28:17 +0530

    Time: 0 ms (100%), Space: 19.2 MB (96.3%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,45 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        n = len(s)
+
+        left_range = -2 ** 31
+        right_range = (2 ** 31) - 1
+        
+        i = 0
+
+        while i < n and s[i] == " ":
+            i+=1
+
+        is_negative = False
+
+        if i > n - 1:
+            return 0
+        
+        if s[i] == "-":
+            is_negative = True
+            i+=1
+        elif s[i] == "+":
+            i+=1
+
+        result = 0
+
+        while i < n:
+            ch = s[i]
+            ch_number = ord(ch)
+
+            if 48 <= ch_number <= 57:
+                number = ch_number - 48
+                result = result * 10 + number
+            else:
+                break
+            i+=1
+
+        result = -result if is_negative else result
+
+        if result > right_range:
+            return right_range
+        elif result < left_range:
+            return left_range
+        else:
+            return result
+
