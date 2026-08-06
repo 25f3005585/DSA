3c8711dd4880facaf86ffeed054f17e6997bc41b
Ayush Dixit <fsayush100@gmail.com>
2026-08-06 16:54:07 +0530

    Time: 119 ms (33.79%), Space: 19.7 MB (73.65%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -8,38 +8,32 @@ class Solution:
 
         string = ""
         min_length = float("inf")
-
-        hash_map_t = {}
-        hash_map_s = {}
+        count = 0
+        hash_map = {}
         left = 0
 
         for i in range(n):
-            hash_map_t[t[i]] = hash_map_t.get(t[i],0) + 1
+            hash_map[t[i]] = hash_map.get(t[i],0) + 1
         
         for right in range(m):
-            hash_map_s[s[right]] = hash_map_s.get(s[right],0) + 1
+            if s[right] in hash_map:
+                if hash_map[s[right]] > 0:
+                    count += 1
+                hash_map[s[right]] -= 1
+            else:
+                hash_map[s[right]] = -1
 
-            is_window_valid = True
-            for key in hash_map_t:
-                if hash_map_s.get(key, 0) < hash_map_t[key]:
-                    is_window_valid = False
-                    break
 
-            while is_window_valid:
+            while count == n:
                 if right - left + 1 < min_length:
                     min_length = right - left + 1
                     string = s[left:right+1]
                 
-                hash_map_s[s[left]] -= 1
-                if hash_map_s[s[left]] == 0:
-                    del hash_map_s[s[left]]
-                left += 1
+                hash_map[s[left]] += 1
+                if s[left] in hash_map and hash_map[s[left]] > 0:
+                    count -= 1
+                left+=1
 
-                for key in hash_map_t:
-                    if hash_map_s.get(key, 0) < hash_map_t[key]:
-                        is_window_valid = False
-                        break
-            
         return string
 
 
