7d490c004ad0ebc540a1a35fa754b3bcf19a21e0
Ayush Dixit <fsayush100@gmail.com>
2026-08-07 17:39:11 +0530

    Time: 55 ms (43.39%), Space: 19.7 MB (85.91%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,35 +1,37 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        k = len(p)
-        n = len(s)
+        n1 = len(s)
+        n2 = len(p)
 
+        if n2 > n1:
+            return []
+        
         arr1 = [0] * 26
         arr2 = [0] * 26
-        ans = []
-
-        for i in range(k):
-            index = ord(p[i]) - ord('a')
-            arr1[index] += 1
 
         left = 0
+        result = []
 
-        for right in range(n):
-            index = ord(s[right]) - ord('a')
+        for i in range(n2):
+            index = ord(p[i]) - ord('a')
             arr2[index] += 1
 
-            if right - left + 1 > k:
+        for right in range(n1):
+            index = ord(s[right]) - ord('a')
+            arr1[index] += 1
+
+            if right - left + 1 > n2:
                 left_index = ord(s[left]) - ord('a')
-                arr2[left_index] -= 1
+                arr1[left_index] -= 1
                 left += 1
-
-            if right - left + 1 == k:
-                is_anagram = True
-                for i in range(26):
-                    if arr1[i] != arr2[i]:
-                        is_anagram = False
-                        break
-                
-                if is_anagram:
-                    ans.append(left)
-
-        return ans
+            
+            is_anagram = True
+            for i in range(26):
+                if arr1[i] != arr2[i]:
+                    is_anagram = False
+                    break
+            
+            if is_anagram:
+                result.append(left)
+        
+        return result
