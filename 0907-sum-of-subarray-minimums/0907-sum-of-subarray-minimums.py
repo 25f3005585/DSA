822b99f8f1e4e1846e612dfa7a152d31ad951a13
Ayush Dixit <fsayush100@gmail.com>
2026-08-26 13:41:02 +0530

    Time: 628 ms (9.57%), Space: 25.2 MB (6.59%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -0,0 +1,55 @@
+class Solution:
+    def next_smaller(self, heights,n):
+        arr = [0] * n
+        stack = []
+
+        for i in range(n-1, -1 , -1):
+            elem = heights[i]
+
+            while stack and stack[-1][0] >= elem:
+                stack.pop()
+            
+            if stack:
+                arr[i] = stack[-1][1]
+            else:
+                arr[i] = -1
+            
+            stack.append((elem, i))
+        return arr
+    
+    def previous_smaller(self,heights, n):
+        arr = [0] * n
+
+        stack = []
+
+        for i in range(n):
+            elem = heights[i]
+
+            while stack and stack[-1][0] > elem:
+                stack.pop()
+            
+            if stack:
+                arr[i] = stack[-1][1]
+            else:
+                arr[i] = -1
+            
+            stack.append((elem,i))
+        return arr
+
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        MOD = 10**9 + 7
+        n = len(arr)
+        total_sum = 0
+
+        previous_elem = self.previous_smaller(arr, n)
+        next_elem = self.next_smaller(arr, n)
+
+        for i in range(n):
+            pr_index = previous_elem[i]
+            next_index = next_elem[i] if next_elem[i] != -1 else n
+
+            contribution = (next_index - i) * (i - pr_index)
+            total_sum += (contribution * arr[i]) % MOD
+        
+        total_sum = total_sum % MOD
+        return total_sum
