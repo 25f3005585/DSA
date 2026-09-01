43dbbedb6fdb1fd8b028c5b499e599076630861f
Ayush Dixit <fsayush100@gmail.com>
2026-09-01 13:08:55 +0530

    Time: 7 ms (29.49%), Space: 21 MB (69.97%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,15 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 1:
-            return nums[0]
+        element = nums[0]
 
-        hash_map = {}
-
-        for i in range(n):
-            if nums[i] in hash_map:
-                hash_map.pop(nums[i])
-            else:
-                hash_map[nums[i]] = True
-
-        return list(hash_map)[0]
+        for i in range(1,n):
+            element ^= nums[i]
+        
+        return element
